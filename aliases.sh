51c41954490eba5acf6aff024c50c09073eae76b
dntrcmp420 <dntrcmp420@damai-e526>
2022-02-10 15:49:45 +0800

    automation scripts added
    made some bash scripts to help me type faster in the CLI.
    Also added it to my ~/bashrc as a temporary aliases.
    Aliases only to be used with this repo.

---
+++ aliases.sh
@@ -0,0 +1,8 @@
+classes_dir='classes/'
+src_dir='src/'
+
+alias jcompile="./scripts/compile.sh"
+alias jclean="./scripts/clean.sh"
+alias jbuild="./scripts/build.sh"
+alias jrun="./scripts/run.sh"
+alias jvi="vi -p . $src_dir/"
