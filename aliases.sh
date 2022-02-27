95d5bf175649bc21bc4d8bb5c5509ad914a3070d
lurkyphish <Lurky-phish-2085@users.noreply.github.com>
2022-02-27 15:39:55 +0800

    FoodItem and BeverageItem for Player and much more...
    Added a docs directory to store my journal while making
    changes to the source code. Better read it to understand
    what happened to this commit.
    
    I also modified some linux automation scripts to be compatible
    with most machines without making it as an executable.

--- aliases.sh
+++ aliases.sh
@@ -1,9 +1,9 @@
 classes_dir='classes/'
 src_dir='src/'
 
-alias jcompile="./scripts/compile.sh"
-alias jclean="./scripts/clean.sh"
-alias jbuild="./scripts/build.sh"
-alias jrun="./scripts/run.sh"
+alias jcompile="sh ./scripts/compile.sh"
+alias jclean="sh ./scripts/clean.sh"
+alias jbuild="sh ./scripts/build.sh"
+alias jrun="sh ./scripts/run.sh"
 alias jvi="vi -p . $src_dir/"
 alias jtree="tree -C $src_dir $classes_dir builds/"
---
+++ src/BeverageItem.java
@@ -0,0 +1,19 @@
+public class BeverageItem extends Item {
+
+	private int quenchLevel;
+
+	public BeverageItem(String name, String description, int price, int quenchLevel) {
+		super(name, description, price);
+		
+		this.quenchLevel = quenchLevel;
+	}
+
+	public int getQuenchLevel() {
+		return quenchLevel;
+	}
+
+	public void setQuenchLevel(int level) {
+		this.quenchLevel = level;
+	}
+
+}
