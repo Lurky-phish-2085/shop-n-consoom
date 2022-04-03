9c822e532d041ae04571f5b642123a1918345b20
dntrcmp420 <dntrcmp420@damai-e526>
2022-04-03 17:23:32 +0800

    ignored another dirs

--- scripts/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-classes_dir='classes/'
-src_dir='src/'
-
-javac -d $classes_dir -sourcepath $src_dir `find $src_dir -name "*.java"`
--- scripts/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-classes_dir='classes/'
-src_dir='src/'
-
-java -cp $classes_dir "$1"
