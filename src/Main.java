ac7de594ee6a657d2d64c5b4fe67069b341fd983
dntrcmp420 <dntrcmp420@damai-e526>
2022-04-26 09:36:38 +0800

    Created a prototype of the game in a messy single Game class
    Right now, I'm in the middle of finishing the mainMenu()

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try {
+			Game.play();
+		} catch (InterruptedException ex) {
+			System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAA");
+		}
+	}
+}
